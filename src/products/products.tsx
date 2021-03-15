d2d865f1206b2de46ea1733966c6cec5eed62815
Bryan <bryanvac@ucm.es>
2021-03-15 13:25:35 +0100

    add component

---
+++ src/products/products.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const productComponent =  () => {
+
+    
+
+    return (
+        <div className="products"> 
+            <h1>Producto: </h1>
+            <img></img>
+        </div>
+    );
+}
+
+export default productComponent;
