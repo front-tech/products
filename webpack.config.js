984291be3199aac382b5569418feee05dec60fdc
Héctor Reyes López <hector.reyes.lopez.17@gmail.com>
2021-03-16 11:58:14 +0100

    new name

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const path = require("path");
 module.exports = (webpackConfigEnv, argv) => {
 	const defaultConfig = singleSpaDefaults({
 		// The name of the organization this application is written for
-		orgName: 'frontech',
+		orgName: 'front-tech',
 
 		// The name of the current project. This usually matches the git repo's name
 		projectName: 'products',
